6e183c9a559abf11580235ea67018720eb280ebe
sudoping01 <sudoping01@gmail.com>
2025-03-17 23:10:12 +0000

    commit
    main init

---
+++ adia_tts/__init__.py
@@ -0,0 +1,7 @@
+
+from .adia_tts import AdiaTTS
+
+__version__ = "0.1.0"
+__author__ = "sudoping01"
+__description__ = "Text-to-Speech synthesis in Wolof language using Adia_TTS model"
+__all__ = ["AdiaTTS"]
