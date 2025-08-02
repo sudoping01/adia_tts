2767fc083e2179a9d06503c38e3423d36d0f37d8
sed <84201601+sudoping01@users.noreply.github.com>
2025-08-02 15:35:36 +0000

    Update __init__.py

--- adia_tts/__init__.py
+++ adia_tts/__init__.py
@@ -1,7 +1,7 @@
 
 from .adia_tts import AdiaTTS
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __author__ = "sudoping01"
 __description__ = "Text-to-Speech synthesis in Wolof language using Adia_TTS model"
 __all__ = ["AdiaTTS"]
